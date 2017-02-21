88f33cadb3ba510819174d2f2588444fab1e5e9e
vanessamnoble <vanessamnoble@icloud.com>
2017-02-21 16:34:57 -0600

    extended the CrudRepository

--- src/main/java/com/codeup/repositories/UsersRepository.java
+++ src/main/java/com/codeup/repositories/UsersRepository.java
@@ -1,10 +1,8 @@
 
-
 /**
- * Created by vanessamnoble on 2/13/17.
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 package com.codeup.repositories;
-
 
 import com.codeup.models.User;
 import org.springframework.data.repository.CrudRepository;
@@ -15,7 +13,5 @@ public interface UsersRepository extends CrudRepository<User, Integer> {
     // select * from user where username = ?
     // automagic
     public User findByUsername(String username);
-
-    User findById(Long id);
 }
 
